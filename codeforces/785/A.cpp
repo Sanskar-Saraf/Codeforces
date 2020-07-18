c1531585d73005a9c7d286e41a1cb6427f33c660
Sanskar Saraf <sanskarsaraf01@gmail.com>
2020-07-18 20:34:00 +0530

    Add solution for problem `A - Anton and Polyhedrons` Link: https://codeforces.com/contest/785/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/785/submission/87242392

---
+++ codeforces/785/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k=0;
+	cin>>n;
+	string s;
+	for(int i=0;i<n;i++)
+	{
+		cin>>s;
+		if(s=="Tetrahedron")
+			k+=4;
+		else if(s=="Cube")
+			k+=6;
+		else if(s=="Octahedron")
+			k+=8;
+		else if(s=="Dodecahedron")
+			k+=12;
+		else if(s=="Icosahedron")
+			k+=20;
+	}
+	cout<<k;
+	return 0;
+}
+
