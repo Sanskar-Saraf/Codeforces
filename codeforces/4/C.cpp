da42910065a1a753840df3dacb5d0cc6c895be22
Sanskar Saraf <sanskarsaraf01@gmail.com>
2021-06-14 14:35:00 +0530

    Add solution for problem `C - Registration System` Link: https://codeforces.com/contest/4/problem/C Tags: data structures, hashing, implementation, *1300 Ref: https://codeforces.com/contest/4/submission/119436532

--- codeforces/4/C.cpp
+++ codeforces/4/C.cpp
@@ -1,39 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define mod 1e9 + 7
-int max(int a, int b)
+int main()
 {
-    if (a > b)
-        return a;
-    return b;
-}
-int min(int a, int b)
-{
-    if (a > b)
-        return b;
-    return a;
-}
-signed main()
-{
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    string s;
-    map<string, int> mp;
-    for (int i = 0; i < t; i++)
-    {
-        cin >> s;
-        if (mp[s] == 0)
-        {
-            cout << "OK\n";
-        }
-        else
-        {
-            cout << s << mp[s] << "\n";
-        }
-        mp[s]++;
-    }
-    return 0;
+	int n,s=0;
+	cin>>n;
+	map<string ,int> mp;
+	string p;
+	for(int i=0;i<n;i++)
+	{
+		cin>>p;
+		if(mp[p]==0)
+		{
+			cout<<"OK\n";
+			mp[p]++;
+		}
+		else
+		{
+			cout<<p<<mp[p]<<"\n";
+			mp[p]++;
+		}
+	}
+	return 0;
 }
