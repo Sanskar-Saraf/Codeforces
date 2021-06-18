6bf12f3e505d80be75dacaf0ecd2f692e0f0f6b3
Sanskar Saraf <sanskarsaraf01@gmail.com>
2021-06-18 20:24:00 +0530

    Add solution for problem `B - Bad Boy` Link: https://codeforces.com/contest/1537/problem/B Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1537/submission/119825195

---
+++ codeforces/1537/B.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll x,y,i,j;
+        cin>>x>>y>>i>>j;
+        cout<<"1 1 "<<x<<" "<<y<<"\n";
+    }
+    return 0;
+}
