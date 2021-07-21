6691105320c8f208faded3e62602ddcc0124f05d
Sanskar Saraf <sanskarsaraf01@gmail.com>
2021-07-21 21:42:00 +0530

    Add solution for problem `A - And Then There Were K` Link: https://codeforces.com/contest/1527/problem/A Tags: bitmasks, *800 Ref: https://codeforces.com/contest/1527/submission/123207375

---
+++ codeforces/1527/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int k=log2(n);
+        ll m=pow(2,k)-1;
+        cout<<m<<"\n";
+    }
+    return 0;
+}
