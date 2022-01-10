84979926c8b6133b8c77c06a41a03849ff289c6d
Sanskar Saraf <sanskarsaraf01@gmail.com>
2022-01-10 20:07:00 +0530

    Add solution for problem `A - Plus One on the Subset` Link: https://codeforces.com/contest/1624/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1624/submission/142189814

---
+++ codeforces/1624/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        sort(arr,arr+n);
+        int ans=arr[n-1]-arr[0];
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
