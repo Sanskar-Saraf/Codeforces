18fe4dc9afc83098ca099ee636389e7d6f0cff7d
Sanskar Saraf <sanskarsaraf01@gmail.com>
2022-01-27 20:09:00 +0530

    Add solution for problem `A - Min Max Swap` Link: https://codeforces.com/contest/1631/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1631/submission/144170235

---
+++ codeforces/1631/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n],b[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            cin>>b[i];
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>b[i])
+                swap(a[i],b[i]);
+        }
+        ll ans=(*max_element(a,a+n))*(*max_element(b,b+n));
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
