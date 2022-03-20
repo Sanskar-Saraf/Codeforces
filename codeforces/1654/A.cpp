3d1ea5021c070ebdbe3dd2904c96c4f03a8b4711
Sanskar Saraf <sanskarsaraf01@gmail.com>
2022-03-20 17:07:00 +0530

    Add solution for problem `A - Maximum Cake Tastiness` Link: https://codeforces.com/contest/1654/problem/A Tags: brute force, greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/1654/submission/150229434

---
+++ codeforces/1654/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define mod 1e9 + 7
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+int min(int a, int b)
+{
+    if (a > b)
+        return b;
+    return a;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+        int ans = arr[n - 1] + arr[n - 2];
+        cout << ans << "\n";
+    }
+    return 0;
+}
