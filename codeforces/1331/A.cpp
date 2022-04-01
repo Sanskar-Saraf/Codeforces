287cb385028173aa2aef3be3cd7223171c619d58
Sanskar Saraf <sanskarsaraf01@gmail.com>
2022-04-01 16:57:00 +0530

    Add solution for problem `A - Is it rated?` Link: https://codeforces.com/contest/1331/problem/A Tags: *special problem Ref: https://codeforces.com/contest/1331/submission/152283428

---
+++ codeforces/1331/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1e9 + 7
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+int min(int a, int b)
+{
+    if (a > b)
+        return b;
+    return a;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "NO"
+         << "\n";
+    return 0;
+}
