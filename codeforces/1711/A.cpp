da1868999d7c2e3ac56ea20ab281640416357d50
Sanskar Saraf <sanskarsaraf01@gmail.com>
2022-07-24 23:18:00 +0530

    Add solution for problem `A - Perfect Permutation` Link: https://codeforces.com/contest/1711/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1711/submission/165592096

---
+++ codeforces/1711/A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define mod 1e9 + 7
+int max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+int min(int a, int b)
+{
+    if (a > b)
+        return b;
+    return a;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 1; i < n; i++)
+        {
+            arr[i - 1] = i + 1;
+        }
+        arr[n - 1] = 1;
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
